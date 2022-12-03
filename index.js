2b232f5028dfbe9342ea4c12bf02aa4fd60ddc4c
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-03 13:04:38 +0300

    4 lesson

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-var str = 'str';
+var isBirthday = true;
+var userName = 'John';
+var age = 40;
+function logBirthday(isBirthday, userName, age) {
+    if (isBirthday) {
+        return "Congrats ".concat(userName, " , your age: ").concat(age + 1);
+    }
+    else {
+        return 'Error';
+    }
+}
--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
-const str = 'str';
-console.log(str);
+const isBirthday: boolean = true;
+const userName: string = 'John';
+const age: number = 40;
+
+
+
+function logBirthday(isBirthday: boolean,userName: string,age: number): string {
+	if(isBirthday) {
+		return `Congrats ${userName} , your age: ${age + 1}`;
+	} else {
+		return 'Error';
+	}
+}
