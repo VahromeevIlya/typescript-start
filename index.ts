bc7afcac9fbc4083a0214db43bbd94272dc32024
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-05 07:05:02 +0300

    done

--- index.ts
+++ index.ts
@@ -9,16 +9,23 @@ const waterUserData = {
 	units: "m3",
 };
 
-const elRate = 0.45;
-const wRate = 2;
+const elRate: number = 0.45;
+const wRate: number = 2;
 
-const monthPayments = [0, 0]; // [electricity, water]
+const monthPayments: number[] = [0, 0]; // [electricity, water]
 
-const calculatePayments = (elData, wData, elRate, wRate) => {
-	if (elData.mode === "double" && elData.readings < 50) {
-		monthPayments[0] = elData.readings * elRate * 0.7;
+const calculatePayments = (
+	{ readings, mode }: { readings: number; mode: string },
+	wData: {
+		readings: number;
+	},
+	elRate: number,
+	wRate: number
+): void => {
+	if (mode === "double" && readings < 50) {
+		monthPayments[0] = readings * elRate * 0.7;
 	} else {
-		monthPayments[0] = elData.readings * elRate;
+		monthPayments[0] = readings * elRate;
 	}
 
 	monthPayments[1] = wData.readings * wRate;
@@ -26,13 +33,24 @@ const calculatePayments = (elData, wData, elRate, wRate) => {
 
 calculatePayments(electricityUserData, waterUserData, elRate, wRate);
 
-const sendInvoice = (monthPayments, electricityUserData, waterUserData) => {
+const sendInvoice = (
+	[el,water]: number[],
+	electricityUserData: {
+		readings: number;
+		units: string;
+	},
+	waterUserData: {
+		readings: number;
+		units: string;
+	}
+): string => {
 	const text = `    Hello!
     This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
-    It will cost: ${monthPayments[0]}€
+    It will cost: ${el}€
     
     This month you used ${waterUserData.readings} ${waterUserData.units} of water
-    It will cost: ${monthPayments[1]}€`;
+    It will cost: ${water}€`;
 
 	return text;
 };
+console.log(sendInvoice(monthPayments, electricityUserData, waterUserData));
