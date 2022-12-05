6532bdeab41b832a98a4f33f4617702a81fc120c
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-05 06:30:15 +0300

    12 lesson

--- index.ts
+++ index.ts
@@ -1,17 +1,32 @@
 const isBirthday: boolean = true;
-const userName: string = 'John';
+const userName: string = "John";
 const age: number = 40;
 
+const userData = {
+	isBirthday: true,
+	userName: "John",
+	age: 40,
+	messages: { error: "Error" },
+};
 
 const createError = (msg: string) => {
-	throw new Error(msg)
-}
+	throw new Error(msg);
+};
 
-function logBirthday(isBirthday: boolean,userName: string,age: number): string {
-	if(isBirthday === true) {
+function logBirthday({
+	isBirthday,
+	userName,
+	age,
+	messages: { error },
+}: {
+	isBirthday: boolean;
+	userName: string;
+	age: number;
+	messages: { error: string };
+}): string {
+	if (isBirthday) {
 		return `Congrats ${userName} , your age: ${age + 1}`;
-	} else if(isBirthday === false) {
-		return 'Too bad';
+	} else {
+		return createError(error);
 	}
-	return createError('Error');
 }
