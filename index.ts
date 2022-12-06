8a52fec42aae281d9fa18663e9c6288059ae14da
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-06 06:25:07 +0300

    init

--- index.ts
+++ index.ts
@@ -1,52 +1,8 @@
-const userData = {
-	isBirthday: true,
-	userName: "John",
-	age: 40,
-	messages: { error: "Error" },
-};
+const msg: string | number = 19;
+const array: string[] | number[] = ['1','a'];
 
-const userDataTupple: [boolean, number, string] = [true, 40, "John"];
-
-const res = userDataTupple.map((d) => `${d} - done`);
-
-const [brithday, age, userName] = res;
-
-const createError = (msg: string) => {
-	throw new Error(msg);
-};
-
-function logBirthday({
-	isBirthday,
-	userName,
-	age,
-	messages: { error },
-}: {
-	isBirthday: boolean;
-	userName: string;
-	age: number;
-	messages: { error: string };
-}): string {
-	if (isBirthday) {
-		return `Congrats ${userName} , your age: ${age + 1}`;
-	} else {
-		return createError(error);
-	}
+function logMsg (msg: string | number): void {
+	console.log(msg);
 }
-logBirthday(userData);
-
-const departmens: string[] = ["dev", "design", "marketing"];
-
-const department = departmens[0];
-
-const nums: number[][] = [
-	[2, 3, 3],
-	[4, 5, 6],
-];
-
-const report = departmens
-	.filter((d: string) => d !== "dev")
-	.map((d: string) => `${d} - done`);
-
-const [first] = report;
-
-console.log(first);
+logMsg(5);
+logMsg('werw');
