b8132bcbf24f7eca9f2e62bd32aec9f9f644f72a
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-06 06:46:40 +0300

    19 lesson

--- index.ts
+++ index.ts
@@ -1,8 +1,46 @@
 const msg: string | number = 19;
-const array: string[] | number[] = ['1','a'];
+const array: string[] | number[] = ["1", "a"];
 
-function logMsg (msg: string | number): void {
-	console.log(msg);
+//function logMsg (msg: string | number): void {
+//	if(typeof msg === 'string') {
+//		console.log(msg.toLowerCase());
+//	} else {
+//		console.log(msg.toExponential());
+//	}
+
+//}
+function logMsg(msg: string[] | number | boolean): void {
+	if (Array.isArray(msg)) {
+		msg.forEach((m) => console.log(m));
+	} else if (typeof msg === "number") {
+		console.log(msg.toExponential());
+	} else {
+		console.log(msg);
+	}
 }
 logMsg(5);
-logMsg('werw');
+
+function printReadings(a: number | string, b: number | boolean): void {
+	if (a === b) {
+		console.log(a);
+	}
+}
+function printReadings2(a: number[] | string): void {
+	console.log(a.slice(0, 3));
+}
+
+function checkReading(readings: {system: number} | {user: number}): void {
+	if('system' in readings) {
+		console.log(readings.system);
+	} else {
+		console.log(readings.user);	
+	}
+}
+
+function logValue(x: string | Date): void {
+	if(x instanceof Date) {
+		console.log(x.getDay());
+	} else {
+		console.log(x);
+	}
+}
