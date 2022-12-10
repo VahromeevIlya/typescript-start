114eaa03aae4f73deae8a14aec621456ff62913b
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-10 11:20:31 +0300

    init

--- index.ts
+++ index.ts
@@ -1,55 +1,20 @@
-const userData = {
-	isBirthday: true,
-	userName: "John",
-	age: 40,
-	messages: { error: "Error" },
-};
 
-const userDataTupple: [boolean, number, string] = [true, 40, "John"];
-
-const res = userDataTupple.map((d) => `${d} - done`);
-
-const [brithday, age, userName] = res;
-
-const createError = (msg: string) => {
-	throw new Error(msg);
-};
-
-function logBirthday({
-	isBirthday,
-	userName,
-	age,
-	messages: { error },
-}: {
-	isBirthday: boolean;
-	userName: string;
-	age: number;
-	messages: { error: string };
-}): string {
-	if (isBirthday) {
-		return `Congrats ${userName} , your age: ${age + 1}`;
-	} else {
-		return createError(error);
-	}
+enum Directions {
+	TOP,
+	RIGHT,
+	BOTTOM,
+	LEFT
 }
-logBirthday(userData);
 
-const departmens: string[] = ["dev", "design", "marketing"];
+enum TimingFunction {
+	EASE = 'ease',
+	EASE_IN = 'ease-in',
+	LINEAR = 'linear'
+}
 
-const department = departmens[0];
-
-const nums: number[][] = [
-	[2, 3, 3],
-	[4, 5, 6],
-];
-
-const report = departmens
-	.filter((d: string) => d !== "dev")
-	.map((d: string) => `${d} - done`);
-
-const [first] = report;
-
-//prepare for lesson 21
-//finish 1/4
-//finish 26 lesson
-//finish 27 lesson
+function frame(elem: string, dir: Directions, tFunc: TimingFunction): void {
+	if(dir === Directions.RIGHT) {
+		console.log(tFunc);
+	}
+}	
+frame('id',Directions.RIGHT,TimingFunction.EASE);
