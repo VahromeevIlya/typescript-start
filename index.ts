959afb4a944cdbc34f0ee4439bc9565b42f9ade4
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-10 11:47:55 +0300

    2

--- index.ts
+++ index.ts
@@ -14,3 +14,38 @@ function fetchData(data: unknown): void {
 	}
 	
 }
+const userData = `{"isBirthday": true, "ageData": 40,"userNameData": "John"}`;
+
+function safeParse(s: string): unknown {
+	return JSON.parse(s);
+}
+
+const data = safeParse(userData);
+
+function transferData(d: unknown): void {
+	if(typeof d === 'string') {
+		console.log(d.toLowerCase());
+	} else if(typeof d === 'object' && d) {
+		console.log(d);
+	} else {
+		console.error('some error')
+	}
+}
+transferData(data);
+
+try {
+	if(1) {
+		throw new Error('error');
+	}
+} catch (error) {
+	if(error instanceof Error) {
+		console.log(error.message);
+	} else if(typeof error === 'string') {
+		console.log(error);
+	}
+}
+
+type T0 = any | unknown;
+type T1 = number | unknown;
+type T2 = any & unknown;
+type T3 = number & unknown;
