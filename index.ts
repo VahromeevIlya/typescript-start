c3b7b6792b8a2a797046b9f82b3d4ca480ba9c10
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-10 11:55:04 +0300

    init

--- index.ts
+++ index.ts
@@ -1,55 +1,21 @@
-const userData = {
-	isBirthday: true,
-	userName: "John",
-	age: 40,
-	messages: { error: "Error" },
+const dataFromControl = {
+	water: 200,
+	el: 350,
 };
 
-const userDataTupple: [boolean, number, string] = [true, 40, "John"];
+function checkReadings(data: typeof dataFromControl): boolean {
+	const dataFromUser = {
+		water: 200,
+		el: 350,
+	};
 
-const res = userDataTupple.map((d) => `${d} - done`);
-
-const [brithday, age, userName] = res;
-
-const createError = (msg: string) => {
-	throw new Error(msg);
-};
-
-function logBirthday({
-	isBirthday,
-	userName,
-	age,
-	messages: { error },
-}: {
-	isBirthday: boolean;
-	userName: string;
-	age: number;
-	messages: { error: string };
-}): string {
-	if (isBirthday) {
-		return `Congrats ${userName} , your age: ${age + 1}`;
+	if(data.el === dataFromUser.el && data.water === dataFromUser.water) {
+		return true
 	} else {
-		return createError(error);
+		return false;
 	}
 }
-logBirthday(userData);
 
-const departmens: string[] = ["dev", "design", "marketing"];
+const PI = 3.14;
 
-const department = departmens[0];
-
-const nums: number[][] = [
-	[2, 3, 3],
-	[4, 5, 6],
-];
-
-const report = departmens
-	.filter((d: string) => d !== "dev")
-	.map((d: string) => `${d} - done`);
-
-const [first] = report;
-
-//prepare for lesson 21
-//finish 1/4
-//finish 26 lesson
-//finish 27 lesson
+let PIClone: typeof PI;
