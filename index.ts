616d7380fa6aa2a7bdb6d2f001c9c56490d41099
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-17 08:50:09 +0300

    finish 47

--- index.ts
+++ index.ts
@@ -1,17 +1,22 @@
-function processingData<T>(data: T): T {
+function processingData<T, S>(data: T[], options: S): string {
+	data.length;
 
-
-
-
-	return data;
+	switch (typeof data) {
+		case "string":
+			return `${data}, speed: ${options}`;
+		case "number":
+			return `${data}, speed: ${options}`;
+		default:
+			return "Not valid";
+	}
 }
 
-const res1 = processingData(1);
-const res2 = processingData("1");
+const res1 = processingData([1], "fast");
+const res2 = processingData(["1"], "slow");
 
 const num = 10;
 
-const res3 = processingData<number>(num);
+const res3 = processingData<number, string>([num], "str");
 
 interface PrintUK {
 	design: number;
@@ -26,5 +31,27 @@ interface Print<T> {
 }
 
 const somePrint: Print<number> = {
-	design: 21
+	design: 21,
+};
+
+interface DataSaver {
+	processing: ProcessingFn;
 }
+
+function processing<T>(data: T): T {
+	return data;
+}
+
+interface ProcessingFn {
+	<T>(data: T): T
+}
+
+let newFunc: ProcessingFn = processing;
+
+const saver: DataSaver = {
+	//processing(data) {
+	//	console.log(data);
+	//	return data;
+	//},
+	processing
+};
