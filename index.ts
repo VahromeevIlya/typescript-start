4a481fa07297a4e0eed5411d21fdafb41a31e02a
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-17 09:13:50 +0300

    finish 48

--- index.ts
+++ index.ts
@@ -1,57 +1,60 @@
-function processingData<T, S>(data: T[], options: S): string {
-	data.length;
-
-	switch (typeof data) {
-		case "string":
-			return `${data}, speed: ${options}`;
-		case "number":
-			return `${data}, speed: ${options}`;
-		default:
-			return "Not valid";
-	}
-}
-
-const res1 = processingData([1], "fast");
-const res2 = processingData(["1"], "slow");
-
-const num = 10;
-
-const res3 = processingData<number, string>([num], "str");
-
-interface PrintUK {
-	design: number;
-}
-
-interface PrintES {
-	design: string;
-}
-
-interface Print<T> {
-	design: T;
-}
-
-const somePrint: Print<number> = {
-	design: 21,
-};
-
-interface DataSaver {
-	processing: ProcessingFn;
+interface ProcessingFn {
+	<T>(data: T): T;
 }
 
 function processing<T>(data: T): T {
 	return data;
 }
 
-interface ProcessingFn {
-	<T>(data: T): T
-}
-
 let newFunc: ProcessingFn = processing;
 
-const saver: DataSaver = {
-	//processing(data) {
-	//	console.log(data);
-	//	return data;
-	//},
-	processing
+type Smth<T> = T;
+
+const number: Smth<number> = 5;
+
+interface Parents {
+	mother: string;
+	father: string;
+}
+
+interface User<ParentsData extends Parents> {
+	login: string;
+	age: number;
+	parents: ParentsData;
+}
+
+const user: User<{ mother: string; father: string,married: boolean }> = {
+	login: "login",
+	age: 25,
+	parents: {
+		mother: "mother",
+		father: "father",
+		married: true
+	},
 };
+
+
+type OrNull<Type> = Type | null;
+
+type OneOrMany<Type> = Type | Type[];
+
+const data: OneOrMany<number[]> = [5];
+
+
+//const depositMoney = <T extends number | string>(amout: T): T => {
+//	console.log(`req to server with amout: ${amout}`);
+//	return amout;
+//}
+//depositMoney(500);
+
+//depositMoney('500');
+//depositMoney(false);
+
+const depositMoney = (amout: number | string): number | string => {
+	console.log(`req to server with amout: ${amout}`);
+	return amout;
+}
+depositMoney(500);
+
+depositMoney('500');
+depositMoney(false);
