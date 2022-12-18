db76b836bf68cfd5dad92d4aee6341a8709b578a
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-18 10:33:27 +0300

    init

--- index.ts
+++ index.ts
@@ -1,64 +1,31 @@
-const userData = {
-	isBirthday: true,
-	userName: "John",
-	age: 40,
-	messages: { error: "Error" },
-};
+class User<T, S> {
+	name: T;
+	age: S;
 
-const userDataTupple: [boolean, number, string] = [true, 40, "John"];
-
-const res = userDataTupple.map((d) => `${d} - done`);
-
-const [brithday, age, userName] = res;
-
-const createError = (msg: string) => {
-	throw new Error(msg);
-};
-
-function logBirthday({
-	isBirthday,
-	userName,
-	age,
-	messages: { error },
-}: {
-	isBirthday: boolean;
-	userName: string;
-	age: number;
-	messages: { error: string };
-}): string {
-	if (isBirthday) {
-		return `Congrats ${userName} , your age: ${age + 1}`;
-	} else {
-		return createError(error);
+	constructor(name: T, age: S) {
+		this.name = name;
+		this.age = age;
 	}
+
+	sayMyFullName<T>(surname: T): string {
+		if(typeof surname !== 'string') {
+			return `I have only name: ${this.name}`
+		} else {
+			return `${this.name} ${surname}`
+		}
+	}
+
 }
-logBirthday(userData);
 
-const departmens: string[] = ["dev", "design", "marketing"];
+const ivan = new User('Ivan',30);
+console.log(ivan.sayMyFullName('Smith'));
 
-const department = departmens[0];
+const nameData = 'Alex';
+const ageData = 25;
 
-const nums: number[][] = [
-	[2, 3, 3],
-	[4, 5, 6],
-];
+const alex  = new User<string,number>(nameData,ageData); 
 
-const report = departmens
-	.filter((d: string) => d !== "dev")
-	.map((d: string) => `${d} - done`);
 
-const [first] = report;
-
-//prepare for lesson 21
-//finish 1/4
-//finish 26 lesson
-//finish 27 lesson
-//finish 33 lesson
-//finish practice 4
-//finish 39
-//finish 41
-//finish 42
-//finish 43
-//finish 44
-//finish 46
-//finish 47 
+class AdminUser<T> extends User<string,number> {
+	roots: T;
+}
