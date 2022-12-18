7ce090aede2519e612675dd505d254e0c93a854d
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-18 10:51:08 +0300

    init

--- index.ts
+++ index.ts
@@ -1,65 +1,31 @@
-const userData = {
-	isBirthday: true,
-	userName: "John",
-	age: 40,
-	messages: { error: "Error" },
-};
+const arr: Array<number> = [1, 2, 3];
+const arr1: number[] = [1, 2, 3];
 
-const userDataTupple: [boolean, number, string] = [true, 40, "John"];
+const readonlyarr: ReadonlyArray<string> = ["123", "32113"];
 
-const res = userDataTupple.map((d) => `${d} - done`);
+interface IState {
+	readonly data: {
+		name: string;
+	};
+	tag?: string;
+}
 
-const [brithday, age, userName] = res;
-
-const createError = (msg: string) => {
-	throw new Error(msg);
-};
-
-function logBirthday({
-	isBirthday,
-	userName,
-	age,
-	messages: { error },
-}: {
-	isBirthday: boolean;
-	userName: string;
-	age: number;
-	messages: { error: string };
-}): string {
-	if (isBirthday) {
-		return `Congrats ${userName} , your age: ${age + 1}`;
-	} else {
-		return createError(error);
+const state: Partial<IState> = {
+	data: {
+		name: 'John'
 	}
 }
-logBirthday(userData);
 
-const departmens: string[] = ["dev", "design", "marketing"];
+strictState.data = 'erwe';
 
-const department = departmens[0];
+const strictState: Required<IState> = {
+	data: {
+		name: "Alex"
+	},
+	tag: 'tag'
+}
 
-const nums: number[][] = [
-	[2, 3, 3],
-	[4, 5, 6],
-];
 
-const report = departmens
-	.filter((d: string) => d !== "dev")
-	.map((d: string) => `${d} - done`);
-
-const [first] = report;
-
-//prepare for lesson 21
-//finish 1/4
-//finish 26 lesson
-//finish 27 lesson
-//finish 33 lesson
-//finish practice 4
-//finish 39
-//finish 41
-//finish 42
-//finish 43
-//finish 44
-//finish 46
-//finish 47 
-//finish 50
+function action(state: Readonly<IState>) {
+	state.data.name = "abs";
+}
