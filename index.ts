fde2689cc6c284cb8b021ae73f52dfe5d0d82e76
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-18 11:05:50 +0300

    init

--- index.ts
+++ index.ts
@@ -1,65 +1,30 @@
-const userData = {
-	isBirthday: true,
-	userName: "John",
-	age: 40,
-	messages: { error: "Error" },
-};
-
-const userDataTupple: [boolean, number, string] = [true, 40, "John"];
-
-const res = userDataTupple.map((d) => `${d} - done`);
-
-const [brithday, age, userName] = res;
-
-const createError = (msg: string) => {
-	throw new Error(msg);
-};
-
-function logBirthday({
-	isBirthday,
-	userName,
-	age,
-	messages: { error },
-}: {
-	isBirthday: boolean;
-	userName: string;
-	age: number;
-	messages: { error: string };
-}): string {
-	if (isBirthday) {
-		return `Congrats ${userName} , your age: ${age + 1}`;
-	} else {
-		return createError(error);
-	}
+interface ICompany {
+	name: string;
+	debts: number;
 }
-logBirthday(userData);
 
-const departmens: string[] = ["dev", "design", "marketing"];
+type CompanyKeys = keyof ICompany;
 
-const department = departmens[0];
+const keys: CompanyKeys = "name";
 
-const nums: number[][] = [
-	[2, 3, 3],
-	[4, 5, 6],
-];
+function printDebts<T, K extends keyof T, S extends keyof T>(
+	company: T,
+	name: K,
+	debts: S
+) {
+	console.log(`Company ${company[name]}, debts: ${company[debts]}`);
+}
 
-const report = departmens
-	.filter((d: string) => d !== "dev")
-	.map((d: string) => `${d} - done`);
+const hh: ICompany = {
+	name: "HH",
+	debts: 500000,
+};
 
-const [first] = report;
+printDebts(hh, "name",'debts');
 
-//prepare for lesson 21
-//finish 1/4
-//finish 26 lesson
-//finish 27 lesson
-//finish 33 lesson
-//finish practice 4
-//finish 39
-//finish 41
-//finish 42
-//finish 43
-//finish 44
-//finish 46
-//finish 47 
-//finish 50
+const google = {
+	name: "Google",
+	open: 'true'
+}
+
+printDebts(google,'name','open');
