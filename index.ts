64763223b5bc01552e0028f19eb750e3cf5208f4
Ilya <ilya.vahromeev@widestudio.ru>
2022-12-19 06:42:16 +0300

    55

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
 interface ICompany {
 	name: string;
 	debts: number;
+	departments: Department[];
+	management: {
+		owner: string;
+	};
 }
+
+interface Department {
+	[key: string]: string;
+}
+
+//const debts = "debts";
+//let debts = "debts" as "debts";
+let debts: "debts" = "debts";
+
+type CompanyDebtsType = ICompany[typeof debts];
+
+//type CompanyDebtsType = ICompany["debts"];
+type CompanyOwnerType = ICompany["management"]["owner"];
+type CompanyDeparmentsType = ICompany["departments"][number];
+type CompanyDeparmentsTypeS = ICompany["departments"];
+type Test = ICompany[keyof ICompany];
 
 type CompanyKeys = keyof ICompany;
 
@@ -15,20 +35,24 @@ function printDebts<T, K extends keyof T, S extends keyof T>(
 	console.log(`Company ${company[name]}, debts: ${company[debts]}`);
 }
 
-const hh: ICompany = {
-	name: "HH",
-	debts: 500000,
+//const hh: ICompany = {
+//	name: "HH",
+//	debts: 500000,
+//};
+
+printDebts(hh, "name", "debts");
+
+const google: ICompany = {
+	name: "Google",
+	debts: 5000,
+	departments: {
+		developer: "dev",
+	},
+	management: {
+		owner: "dev",
+	},
 };
 
-printDebts(hh, "name",'debts');
-
-const google = {
-	name: "Google",
-	open: 'true'
-}
-
-printDebts(google,'name','open');
+printDebts(google, "name", "debts");
 
 type GoogleKeys = keyof typeof google;
-
-const keys2: GoogleKeys = 'open';
